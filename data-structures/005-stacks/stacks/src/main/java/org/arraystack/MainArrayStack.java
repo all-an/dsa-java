d1b540dbb1de9f50ef9ec424029c44003c389efe
all-an <all-an@hotmail.com>
2023-07-16 17:51:53 -0300

    increasing stack size

--- data-structures/005-stacks/stacks/src/main/java/org/arraystack/MainArrayStack.java
+++ data-structures/005-stacks/stacks/src/main/java/org/arraystack/MainArrayStack.java
@@ -10,6 +10,14 @@ public class MainArrayStack {
 		stack.push(new Employee("Mary", "Smith", 22));
 		stack.push(new Employee("Mike", "Wilson", 3245));
 		stack.push(new Employee("Bill", "End", 78));
+		stack.push(new Employee("6", "6", 6));
+		stack.push(new Employee("7", "7", 7));
+		stack.push(new Employee("8", "8", 8));
+		stack.push(new Employee("9", "9", 9));
+		stack.push(new Employee("10", "10", 10));
+		stack.push(new Employee("11", "11", 11));
+		stack.push(new Employee("12", "12", 12));
+		stack.push(new Employee("13", "13", 13));
 
 		//stack.printStack();
 
