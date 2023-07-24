49c20ab451f6e8ba40d62a6db6ce3e2a93bf6de3
all-an <all-an@hotmail.com>
2023-07-24 18:54:58 -0300

    finish circular queue

--- data-structures/006-queues/002-circular-queue/circularqueue/src/main/java/org/example/Main.java
+++ data-structures/006-queues/002-circular-queue/circularqueue/src/main/java/org/example/Main.java
@@ -3,26 +3,26 @@ package org.example;
 public class Main {
 	public static void main(String[] args) {
 
-			Employee janeJones = new Employee("Jane", "Jones", 123);
-			Employee johnDoe = new Employee("John", "Doe", 4567);
-			Employee marySmith = new Employee("Mary", "Smith", 22);
-			Employee mikeWilson = new Employee("Mike", "Wilson", 3245);
-			Employee billEnd = new Employee("Bill", "End", 78);
+		Employee janeJones = new Employee("Jane", "Jones", 123);
+		Employee johnDoe = new Employee("John", "Doe", 4567);
+		Employee marySmith = new Employee("Mary", "Smith", 22);
+		Employee mikeWilson = new Employee("Mike", "Wilson", 3245);
+		Employee billEnd = new Employee("Bill", "End", 78);
 
-			ArrayQueue queue = new ArrayQueue(5);
+		ArrayQueue queue = new ArrayQueue(5);
 
-			queue.add(janeJones);
-			queue.add(johnDoe);
-			queue.remove();
-			queue.add(marySmith);
-			queue.remove();
-			queue.add(mikeWilson);
-			queue.remove();
-			queue.add(billEnd);
-			queue.remove();
-			queue.add(janeJones);
+		queue.add(janeJones);
+		queue.add(johnDoe);
+		queue.remove();
+		queue.add(marySmith);
+		queue.remove();
+		queue.add(mikeWilson);
+		queue.remove();
+		queue.add(billEnd);
+		queue.remove();
+		queue.add(janeJones);
 
-			queue.printQueue();
+		queue.printQueue();
 
 //        queue.add(janeJones);
 //        queue.add(johnDoe);
@@ -40,12 +40,12 @@ public class Main {
 //        queue.remove();
 //        queue.remove();
 //        //queue.remove();
-//
+////
 //        queue.add(mikeWilson);
-//
-//
+////
+////
 //        queue.printQueue();
 
 
-		}
 	}
+}
