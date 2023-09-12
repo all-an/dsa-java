571685ac3879188973cec08bf9099c64f958c8b4
all-an <all-an@hotmail.com>
2023-09-12 18:35:42 -0300

    "node to delete has 2 childre"

--- algorithms/003-searching/003-binary-search-trees/binarysearchtrees/src/main/java/org/binarysearchtrees/Tree.java
+++ algorithms/003-searching/003-binary-search-trees/binarysearchtrees/src/main/java/org/binarysearchtrees/Tree.java
@@ -40,28 +40,37 @@ public class Tree {
 	}
 
 	private TreeNode delete(TreeNode subtreeRoot, int value) {
-		if (subtreeRoot == null) {
-			return subtreeRoot;
-		}
+        if (subtreeRoot == null) {
+            return subtreeRoot;
+        }
 
-		if (value < subtreeRoot.getData()) {
-			subtreeRoot.setLeftChild(delete(subtreeRoot.getLeftChild(), value));
-		}
-		else if (value > subtreeRoot.getData()) {
-			subtreeRoot.setRightChild(delete(subtreeRoot.getRightChild(), value));
-		}
-		else {
-			// Cases 1 and 2: node to delete has 0 or 1 child(ren)
-			if (subtreeRoot.getLeftChild() == null) {
-				return subtreeRoot.getRightChild();
-			}
-			else if (subtreeRoot.getRightChild() == null) {
-				return subtreeRoot.getLeftChild();
-			}
-		}
+        if (value < subtreeRoot.getData()) {
+            subtreeRoot.setLeftChild(delete(subtreeRoot.getLeftChild(), value));
+        }
+        else if (value > subtreeRoot.getData()) {
+            subtreeRoot.setRightChild(delete(subtreeRoot.getRightChild(), value));
+        }
+        else {
+            // Cases 1 and 2: node to delete has 0 or 1 child(ren)
+            if (subtreeRoot.getLeftChild() == null) {
+                return subtreeRoot.getRightChild();
+            }
+            else if (subtreeRoot.getRightChild() == null) {
+                return subtreeRoot.getLeftChild();
+            }
 
-		return subtreeRoot;
-	}
+            // Case 3: node to delete has 2 children
+
+            // Replace the value in the subtreeRoot node with the smallest value
+            // from the right subtree
+            subtreeRoot.setData(subtreeRoot.getRightChild().min());
+
+            // Delete the node that has the smallest value in the right subtree
+            subtreeRoot.setRightChild(delete(subtreeRoot.getRightChild(), subtreeRoot.getData()));
+        }
+
+        return subtreeRoot;
+    }
 
 	public int min() {
 		if (root == null) {
