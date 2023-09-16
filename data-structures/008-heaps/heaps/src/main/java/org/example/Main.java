065b2ed0d70c39d3b26fbe622158073142a41daa
all-an <all-an@hotmail.com>
2023-09-16 17:26:27 -0300

    delete element in the heap

--- data-structures/008-heaps/heaps/src/main/java/org/example/Main.java
+++ data-structures/008-heaps/heaps/src/main/java/org/example/Main.java
@@ -5,5 +5,21 @@ public class Main {
         Heap myHeap = new Heap(5);
         myHeap.insert(42);
         System.out.println(myHeap.getParent(0));
+
+        Heap heap = new Heap(10);
+
+        heap.insert(80);
+        heap.insert(75);
+        heap.insert(60);
+        heap.insert(68);
+        heap.insert(55);
+        heap.insert(40);
+        heap.insert(52);
+        heap.insert(67);
+
+        heap.printHeap();
+
+        heap.delete(0);
+        heap.printHeap();
     }
 }
