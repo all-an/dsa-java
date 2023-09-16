715ca3a561b250b4990bc4d89e52d5368361775d
all-an <all-an@hotmail.com>
2023-09-16 18:51:53 -0300

    priority queues

--- data-structures/008-heaps/heaps/src/main/java/org/heaps/Heap.java
+++ data-structures/008-heaps/heaps/src/main/java/org/heaps/Heap.java
@@ -1,4 +1,4 @@
-package org.example;
+package org.heaps;
 
 public class Heap {
 
--- data-structures/008-heaps/heaps/src/main/java/org/heaps/Main.java
+++ data-structures/008-heaps/heaps/src/main/java/org/heaps/Main.java
@@ -1,4 +1,4 @@
-package org.example;
+package org.heaps;
 
 public class Main {
     public static void main(String[] args) {
